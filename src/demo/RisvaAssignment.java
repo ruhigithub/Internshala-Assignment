c1878a99e0f2a7815dd1a81d99bd14cbe0a5e413
ruhigithub <116343800+ruhigithub@users.noreply.github.com>
2023-07-03 11:32:51 -0700

    Update RisvaAssignment.java

--- src/demo/RisvaAssignment.java
+++ src/demo/RisvaAssignment.java
@@ -28,7 +28,7 @@ package demo;
 		Actions act=new Actions(driver);
 		act.moveToElement(ele1).click().perform();
 
-		   driver.findElement(By.xpath("//div[div[span[contains(text(),'Band Material')]]]/ul[7]/span[3]/li/span/a/span")).click();
+		   driver.findElement(By.xpath("//div[div[span[contains(text(),'Band Material')]]]/ul[7]/span[3]/li/span/a/div/label/i")).click();
 		   
 		   driver.findElement(By.xpath("//div[div[div[span[contains(text(),'Customer Reviews')]]]]/div[4]/ul[1]/span[1]/li/span/a/span")).click();
 		   
@@ -36,7 +36,7 @@ package demo;
 		   driver.findElement(By.xpath("//span[contains(text(),'25% Off or more')]")).click();
 		   
 		   Thread.sleep(2500);
-		   WebElement FifthEle= driver.findElement(By.xpath("//div[@cel_widget_id='MAIN-SEARCH_RESULTS-7']/div[1]/div[1]/div[2]/div[1]"));
+		   WebElement FifthEle= driver.findElement(By.xpath("//div[@cel_widget_id='MAIN-SEARCH_RESULTS-6']/div[1]/div[1]/div[2]/div[1]"));
 		   
 		   System.out.println("The Fifth elment is "+ FifthEle.getText());
 
